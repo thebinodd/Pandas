2a2231dc40d085cd788f671c9fdf4906b4c3c3c4
Binod Sharma <thebinod404@gmail.com>
2026-05-30 21:29:16 +0545

    Introduction to Pandas

---
+++ introduction.py
@@ -0,0 +1,10 @@
+#Pandas is a software library written for the Python programming language for data manipulation and analysis. In particular, it offers data structures and operations for manipulating numerical tables and time series. 
+
+#Data Structures in Numpy -> n-d Arrays
+
+#Data Structures in Pandas -> Series and DataFrames
+
+
+#DataFrame is a 2-dimensional labeled data structure with columns of potentially different types. You can think of it like a spreadsheet or SQL table, or a dict of Series objects. It is generally the most commonly used pandas object.
+
+#Series is a one-dimensional labeled array capable of holding any data type (integers, strings, floating point numbers, Python objects, etc.). The axis labels are collectively referred to as the index.
